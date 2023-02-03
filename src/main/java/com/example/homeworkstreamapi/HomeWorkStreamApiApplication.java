ae7f6816615908d863e4a559e829ec8f65693c17
eduard023 <edo.avdzh@yandex.ru>
2023-02-03 20:39:11 +0300

    Initial commit

---
+++ src/main/java/com/example/homeworkstreamapi/HomeWorkStreamApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.homeworkstreamapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWorkStreamApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeWorkStreamApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/homeworkstreamapi/HomeWorkStreamApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homeworkstreamapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWorkStreamApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
